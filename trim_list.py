d881a1b5361c04b4893a8c4363d7ef18e9075a03
Jace <54625750+Jacekun@users.noreply.github.com>
2020-12-25 15:10:56 +0800

    Added score stat to 'trim_list.py'.

--- trim_list.py
+++ trim_list.py
@@ -18,10 +18,15 @@ inputAnime = "anime.json"
 inputManga = "manga.json"
 outputAnime = "anime_NotInMal.json"
 outputManga = "manga_NotInMal.json"
+outputStats = "animemanga_stats.txt"
 
 # App Properties
 appVersion = '1.0.0.0'
 appBuild = 1
+
+# STATS variables
+statScoreTotal = 0
+statScoreCount = 0
 
 # json objects
 logger("Loading " + inputAnime + " into memory..")
@@ -58,10 +63,26 @@ for entry in jsonAnime:
         # Append to JSON object
         jsonOutputAnime.append(jsonData)
 
+    # Stats checker
+    statScore = int(entry["score"])
+    if (statScore > 0):
+        statScoreTotal = statScoreTotal + statScore
+        statScoreCount = statScoreCount + 1
+
+# Write 'outputAnime'
 logger("Writing file: " + outputAnime)
 with open(outputAnime, "w+", encoding='utf-8') as F:
     F.write(json.dumps(jsonOutputAnime, ensure_ascii=False, indent=4).encode('utf8').decode())
     logger("File generated: " + outputAnime)
+
+# Write stats for Anime
+logger("Appending to file (Average Score stats): " + outputStats)
+with open(outputStats, "a+", encoding='utf-8') as F:
+    F.write("Anime stats:\nAverage Score (out of 100): " + str((statScoreTotal/statScoreCount)*10) + "\n")
+
+# Reset vars
+statScoreTotal = 0
+statScoreCount = 0
 
 # For MANGA
 for entry in jsonManga:
@@ -87,7 +108,19 @@ for entry in jsonManga:
         # Append to JSON object
         jsonOutputManga.append(jsonData)
 
+    # Stats checker
+    statScore = int(entry["score"])
+    if (statScore > 0):
+        statScoreTotal = statScoreTotal + statScore
+        statScoreCount = statScoreCount + 1
+
+# Write 'outputManga'
 logger("Writing file: " + outputManga)
 with open(outputManga, "w+", encoding='utf-8') as F:
     F.write(json.dumps(jsonOutputManga, ensure_ascii=False, indent=4).encode('utf8').decode())
     logger("File generated: " + outputManga)
+
+# Write stats for Manga
+logger("Appending to file (Average Score stats): " + outputStats)
+with open(outputStats, "a+", encoding='utf-8') as F:
+    F.write("Manga stats:\nAverage Score (out of 100): " + str((statScoreTotal/statScoreCount)*10) + "\n")
