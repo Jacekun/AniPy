10f06f3105465b8df89485a7d83be8e34f5f6a84
Jace <54625750+Jacekun@users.noreply.github.com>
2021-01-09 12:37:11 +0800

    Update build.cmd

--- build.cmd
+++ build.cmd
@@ -1,1 +1,1 @@
-pyinstaller --onefile --hidden-import=func.func --hidden-import=func.anilist_request --add-data "main_win.ui;files" main_win.py
+pyinstaller --onefile --hidden-import=func.main --hidden-import=func.anilist_request --hidden-import=func.anilist_getAnime.py --hidden-import=func.anilist_getManga.py --add-data "main_win.ui;files" main_win.py
