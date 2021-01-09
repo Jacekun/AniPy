2ce81532f64c8e408b855d811e3cfa232408c3e9
Jace <54625750+Jacekun@users.noreply.github.com>
2021-01-09 12:57:27 +0800

    main_win: Changed code, to fit main script changes.

--- main_win.py
+++ main_win.py
@@ -12,6 +12,14 @@ from datetime import datetime
 def logger(text):
   print("[" + '{0:%H:%M:%S}'.format(datetime.now()) + "]: " + text)
 
+# Import Dynamic scripts
+logger("Import scripts from same folder")
+fMain = importlib.import_module("func.main")
+fReq = importlib.import_module("func.anilist_request")
+fGetAnime = importlib.import_module("func.anilist_getAnime")
+fGetManga = importlib.import_module("func.anilist_getManga")
+fTrim = importlib.import_module("func.trim_list")
+
 # Global Vars
 logger("Define Global Vars..")
 # Paths for Files
@@ -31,12 +39,8 @@ outputAnimeTrim = os.path.join(PROJECT_PATH, "output\\anime_NotInMal.json")
 outputMangaTrim = os.path.join(PROJECT_PATH, "output\\manga_NotInMal.json")
 outputStats = os.path.join(PROJECT_PATH, "output\\animemanga_stats.txt")
 
-logger("Import scripts from same folder")
-fMain = importlib.import_module("func.main")
-fReq = importlib.import_module("func.anilist_request")
-
 # App Properties
-appVersion = '1.1.0.2'
+appVersion = '1.1.0.3'
 appBuild = 2
 
 # Objects
@@ -81,237 +85,19 @@ class Main:
 
         # Request anime list
         labelStatus["text"] = fMain.logString("Requesting anime list..")
-        # Log duplicate entries
-        fMain.write_append(entryLog, "ANIME Entries Log\n")
-        entryID.clear() # Clear list
+        fGetAnime.getAnimeEntries(userID, username, outputAnime, xmlAnime, entryLog)
 
-        # Check if not existing
-        if not (os.path.exists(outputAnime)):
-          # Get JSON object
-          jsonAnime = fReq.anilist_userlist(userID, "ANIME")
-          labelStatus["text"] = fMain.logString("Checking anime list..")
-
-          # Check if not null
-          if jsonAnime is not None:
-            listAnime = jsonAnime["data"]["MediaListCollection"]["lists"]
-            labelStatus["text"] = fMain.logString("User Anime list received!")
-
-            # Create vars
-            # Count Manga entries
-            cTotal = 0
-            cWatch = 0
-            cComplete = 0
-            cHold = 0
-            cDrop = 0
-            cPtw = 0
-
-            # Start generating JSON and XML..
-            labelStatus["text"] = fMain.logString("Start generating Anime JSON/XML file..")
-
-            # Write to json file
-            fMain.write_append(outputAnime, '[\n')
-
-            # Iterate over the MediaCollection List
-            for anime in listAnime:
-              animeInfo = anime["entries"]
-              # Iterate over the anime information, inside the entries
-              for entry in animeInfo:
-                # Get Anilist ID
-                anilistID = entry["media"]["id"]
-                # Get Anilist Status
-                AnilistStatus = fMain.validateStr(entry["status"])
-
-                # Check if already exists
-                if anilistID in entryID:
-                  fMain.write_append(entryLog, "Skipped: " + str(anilistID) + ", Duplicate Anime entry.\n")
-                  continue
-                else:
-                  entryID.append(anilistID)
-
-                # Write to json file
-                jsontoAdd = fMain.entry_json(entry, 'anime')
-                fMain.write_append(outputAnime, jsontoAdd)
-
-                # Write to MAL Xml File
-                malID = fMain.validateInt(entry["media"]["idMal"])
-                if malID != '0':
-                  # Get XML strings
-                  xmltoWrite = fMain.entry_animexml(malID, entry)
-                  # Write to xml file
-                  fMain.write_append(xmlAnime, xmltoWrite)
-                
-                  # Add count
-                  if (AnilistStatus == "COMPLETED"):
-                    cComplete = cComplete + 1
-                  elif (AnilistStatus == "PAUSED"):
-                    cHold = cHold + 1
-                  elif (AnilistStatus == "CURRENT"):
-                    cWatch = cWatch + 1
-                  elif (AnilistStatus == "DROPPED"):
-                    cDrop = cDrop + 1
-                  elif (AnilistStatus == "PLANNING"):
-                    cPtw = cPtw + 1
-                  elif (AnilistStatus == "REPEATING"):
-                    cWatch = cWatch + 1
-
-            # Delete last comma ',', in json file
-            labelStatus["text"] = "Finalizing Anime JSON file.."
-            logger("Remove last comma from Anime JSON file..")
-            fMain.write_remove(outputAnime, 3)
-
-            # Write ']' at the end, to json file
-            logger("Write last ']' to Anime JSON file..")
-            fMain.write_append(outputAnime, '\n]')
-            logger("Done with Anime JSON file..")
-            
-            # Write to MAL xml file
-            labelStatus["text"] = "Finalizing Anime XML file.."
-            logger("Add closing tag to Anime XML file..")
-            fMain.write_append(xmlAnime, '</myanimelist>')
-
-            # Total counts
-            cTotal = cWatch + cComplete + cHold + cDrop + cPtw
-            logger("Prepend 'myinfo' to Anime XML file..")
-            malprepend = '<?xml version="1.0" encoding="UTF-8" ?>\n<myanimelist>\n'
-            malprepend += '\t<myinfo>\n'
-            malprepend += '\t\t' + fMain.toMalval('', 'user_id') + '\n'
-            malprepend += '\t\t' + fMain.toMalval(username, 'user_name') + '\n'
-            malprepend += '\t\t' + fMain.toMalval('1', 'user_export_type') + '\n'
-            malprepend += '\t\t' + fMain.toMalval(str(cTotal), 'user_total_anime') + '\n'
-            malprepend += '\t\t' + fMain.toMalval(str(cWatch), 'user_total_watching') + '\n'
-            malprepend += '\t\t' + fMain.toMalval(str(cComplete), 'user_total_completed') + '\n'
-            malprepend += '\t\t' + fMain.toMalval(str(cHold), 'user_total_onhold') + '\n'
-            malprepend += '\t\t' + fMain.toMalval(str(cDrop), 'user_total_dropped') + '\n'
-            malprepend += '\t\t' + fMain.toMalval(str(cPtw), 'user_total_plantowatch') + '\n'
-            malprepend += '\t</myinfo>\n'
-            fMain.line_prepender(xmlAnime, malprepend)
-            logger("Done with Anime XML file..")
-
-            # Done anime
-            labelStatus["text"] = "Anime list JSON/XML generated!"
-            logger("Done! File generated: " + outputAnime)
-            logger("Done! File generated: " + xmlAnime)
-
-        # Already existing!
-        else:
-          logger(outputAnime + " file already exist!")
-        
         # Request manga list
         labelStatus["text"] = fMain.logString("Requesting manga list..")
-        # Log duplicate entries
-        fMain.write_append(entryLog, "MANGA Entries Log\n")
-        entryID.clear() # Clear list
+        fGetManga.getMangaEntries(userID, username, outputManga, xmlManga, entryLog)
 
-        # Check if not existing
-        if not (os.path.exists(outputManga)):
-          # Get JSON object
-          jsonManga = fReq.anilist_userlist(userID, "MANGA")
-          labelStatus["text"] = fMain.logString("Checking manga list..")
+        # Trim List
+        labelStatus["text"] = fMain.logString("Trimming list..")
+        fTrim.trim_results(outputAnime, outputManga, outputAnimeTrim, outputMangaTrim, outputStats)
 
-          # Check if not null
-          if jsonManga is not None:
-            listManga = jsonManga["data"]["MediaListCollection"]["lists"]
-            labelStatus["text"] = fMain.logString("User Manga list received!")
-
-            # Create vars
-            # Count Manga entries
-            cTotal = 0
-            cRead = 0
-            cComplete = 0
-            cHold = 0
-            cDrop = 0
-            cPtr = 0
-
-            # Start generating JSON and XML..
-            labelStatus["text"] = fMain.logString("Start generating Manga JSON/XML file..")
-
-            # Write to json file
-            fMain.write_append(outputManga, '[\n')
-
-            # Iterate over the MediaCollection List
-            for manga in listManga:
-              mangaInfo = manga["entries"]
-              # Iterate over the manga information, inside the entries
-              for entry in mangaInfo:
-                # Get Anilist ID
-                anilistID = entry["media"]["id"]
-                # Get Anilist Status
-                AnilistStatus = fMain.validateStr(entry["status"])
-
-                # Check if already exists
-                if anilistID in entryID:
-                  fMain.write_append(entryLog, "Skipped: " + str(anilistID) + ", Duplicate Manga entry.\n")
-                  continue
-                else:
-                  entryID.append(anilistID)
-
-                # Write to json file
-                jsontoAdd = fMain.entry_json(entry, 'manga')
-                fMain.write_append(outputManga, jsontoAdd)
-
-                # Write to MAL Xml File
-                malID = fMain.validateInt(entry["media"]["idMal"])
-                if malID != '0':
-                  # Get XML strings
-                  xmltoWrite = fMain.entry_mangaxml(malID, entry, str(AnilistStatus))
-                  # Write to xml file
-                  fMain.write_append(xmlManga, xmltoWrite)
-
-                  # Add count
-                  if (AnilistStatus == "COMPLETED"):
-                    cComplete = cComplete + 1
-                  elif (AnilistStatus == "PAUSED"):
-                    cHold = cHold + 1
-                  elif (AnilistStatus == "CURRENT"):
-                    cRead = cRead + 1
-                  elif (AnilistStatus == "DROPPED"):
-                    cDrop = cDrop + 1
-                  elif (AnilistStatus == "PLANNING"):
-                    cPtr = cPtr + 1
-                  elif (AnilistStatus == "REPEATING"):
-                    cRead = cRead + 1
-
-          # Delete last comma ',', in json file
-          labelStatus["text"] = "Finalizing Manga JSON file.."
-          logger("Remove last comma from manga JSON file..")
-          fMain.write_remove(outputManga, 3)
-
-          # Write ']' at the end, to json file
-          logger("Write last ']' to manga JSON file..")
-          fMain.write_append(outputManga, '\n]')
-          logger("Done with Manga JSON file..")
-          
-          # Write to MAL xml file
-          labelStatus["text"] = "Finalizing Manga XML file.."
-          logger("Add closing tag..")
-          fMain.write_append(xmlManga, '</myanimelist>')
-
-          # Total counts
-          cTotal = cRead + cComplete + cHold + cDrop + cPtr
-          logger("Prepend 'myinfo' to Manga XML file..")
-          malprepend = '<?xml version="1.0" encoding="UTF-8" ?>\n<myanimelist>\n'
-          malprepend += '\t<myinfo>\n'
-          malprepend += '\t\t' + fMain.toMalval('', 'user_id') + '\n'
-          malprepend += '\t\t' + fMain.toMalval(username, 'user_name') + '\n'
-          malprepend += '\t\t' + fMain.toMalval('2', 'user_export_type') + '\n'
-          malprepend += '\t\t' + fMain.toMalval(str(cTotal), 'user_total_manga') + '\n'
-          malprepend += '\t\t' + fMain.toMalval(str(cRead), 'user_total_reading') + '\n'
-          malprepend += '\t\t' + fMain.toMalval(str(cComplete), 'user_total_completed') + '\n'
-          malprepend += '\t\t' + fMain.toMalval(str(cHold), 'user_total_onhold') + '\n'
-          malprepend += '\t\t' + fMain.toMalval(str(cDrop), 'user_total_dropped') + '\n'
-          malprepend += '\t\t' + fMain.toMalval(str(cPtr), 'user_total_plantoread') + '\n'
-          malprepend += '\t</myinfo>\n'
-          fMain.line_prepender(xmlManga, malprepend)
-          logger("Done with Manga XML file..")
-
-          # Done manga
-          labelStatus["text"] = "Manga list JSON/XML generated!"
-          logger("Done! File generated: " + outputManga)
-          logger("Done! File generated: " + xmlManga)
-
-        else:
-          logger(outputManga + " file already exist!")
-
+        # Done
+        labelStatus["text"] = fMain.logString("Done!")
+       
     else:
       logger("Username is empty!")
 
